e5f01d57fac920ebb7e0c93975cf40e8c7eda095
Wayne del Corral Jr <wdelcorjr@gmail.com>
2020-08-08 20:55:40 -0500

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,5 @@
 def starts_with_a_vowel?(word)
-    vow = word.match(/\A[aeiouAEIOU]/)   
-    !!vow
+    word.match(/\A[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -12,8 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-    match = text.match(/\A[A-Z].+[.!?,;]$/)
-    !!match
+    text.match(/\A[A-Z].+[.!?,;]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
