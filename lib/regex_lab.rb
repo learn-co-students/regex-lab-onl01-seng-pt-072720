729481e521631570af0e02755177ccdfe80d304c
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-09 06:27:03 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,8 +19,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match?((/(\d+)-(\d+)-(\d+)/)|(/\d+/)) 
- 
-  #phone.match?(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+  phone.match?(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
   
 end
