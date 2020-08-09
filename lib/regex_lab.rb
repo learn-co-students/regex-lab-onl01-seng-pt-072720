cf7c824f50df09c5892d92879550a586128512e2
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-09 23:48:13 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
-def starts_with_a_vowel?(word)
+# def starts_with_a_vowel?(word)
+#     if word.match(/\b[aeiouAEIOU]/) == nil
+#     false
+#     else
+#       true
+#   end 
+# end
 
-end
+# def words_starting_with_un_and_ending_with_ing(text)
+#   text.scan(/\w\w+ing\b/)
+# end
 
-def words_starting_with_un_and_ending_with_ing(text)
-
-end
-
-def words_five_letters_long(text)
-
-end
+# def words_five_letters_long(text)
+#   text.scan(/\b\w{5}\b/)
+# end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
-
-def valid_phone_number?(phone)
-
+  if text.match(/\A[A-Z].+[.,!;:]\z/) 
+    true
+  else
+    false
 end
