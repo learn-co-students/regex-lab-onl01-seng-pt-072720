ef3d251b9fad3fc84fef08a9ce90c0cb258f9f35
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-09 06:23:29 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,27 @@
 def starts_with_a_vowel?(word)
-
+  word.capitalize.match?(/\A[AEIOU]/)
+  
+  # !!word.capitalize.match(/\A[AEIOU]/) will also work. "!!" makes it return true or false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  #text.scan(/\b[Uu]*ing\b/)
+  text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  #text.match?(/\A[A-Z].*\W\z/) \A & \z are end-of-string anchors and would work too if it is a one line string
+  text.match?(/^[A-Z].*\W$/) # \^ and $ are end of line anchors
 end
 
 def valid_phone_number?(phone)
-
+  #phone.match?(/\d+-\d+-\d+/)
+  #phone.match?(/\b[0-9]{10}\b/)
+  #phone.match?(/\b^([0-9]{10})$\b/)
+  phone.match?(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+  
 end
