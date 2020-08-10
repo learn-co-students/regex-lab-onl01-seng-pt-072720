cef264f651ce87ee126b99b0d750ce466b0a7516
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-10 00:49:00 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,18 +1,18 @@
-# def starts_with_a_vowel?(word)
-#     if word.match(/\b[aeiouAEIOU]/) == nil
-#     false
-#     else
-#       true
-#   end 
-# end
+def starts_with_a_vowel?(word)
+    if word.match(/\b[aeiouAEIOU]/) == nil
+    false
+    else
+      true
+  end 
+end
 
-# def words_starting_with_un_and_ending_with_ing(text)
-#   text.scan(/\w\w+ing\b/)
-# end
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\w\w+ing\b/)
+end
 
-# def words_five_letters_long(text)
-#   text.scan(/\b\w{5}\b/)
-# end
+def words_five_letters_long(text)
+  text.scan(/\b\w{5}\b/)
+end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/\A[A-Z].+[.,!;:]\z/) 
@@ -20,3 +20,18 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
   else
     false
 end
+end 
+
+def valid_phone_number?(phone)
+  if phone.match(/[0-9]{10}/)
+    true
+  elsif phone.match(/[(][0-9]{3}[)].+[0-9]{3}-[0-9]{4}/)
+    true 
+  elsif phone.match(/[0-9]{3}.+[0-9]{3}.+[0-9]{4}/)
+    true
+  elsif phone.match(/[(][0-9]{3}[)]+[0-9]{7}/)
+    true
+  else 
+    false
+  end
+end 
