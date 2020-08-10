3d5857af9fcbc3be977c5e85df2113d73c77d281
Rebecca Congi <rncongi@gmail.com>
2020-08-10 21:38:33 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,24 +8,12 @@ end
 
 
 def words_starting_with_un_and_ending_with_ing(text)
-  
   text.scan(/\bun\w+ing\b/)
-  
 end
 
 
 def words_five_letters_long(text)
-  new_array = []
-  x = text.split
-  x.each do |word|
-    if word.length == 5
-      new_array << word 
-    end 
-  end
-  new_array
-  #text.split.grep(/^\w{5}\s/)
-   # 10. #Get items from array where first name has five letters:
- # names.grep(/^\w{5}\s/)
+ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
